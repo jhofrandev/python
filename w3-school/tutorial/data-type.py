5d4f0c3e491e1b052678021c0f73755fe1ba8bac
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-06 11:25:02 -0500

    data type

---
+++ w3-school/tutorial/data-type.py
@@ -0,0 +1,8 @@
+# Tipo de texto:	str
+# Tipos numéricos:	int, float, complex
+# Tipos de secuencia:	list, tuple, range
+# Tipo de mapeo:	dict
+# Tipos de conjuntos:	set,frozenset
+# Tipo booleano:	bool
+# Tipos binarios:	bytes, bytearray, memoryview
+# Ninguno Tipo:	NoneType
