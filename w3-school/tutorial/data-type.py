245c6694eca2887b9a952e12acc84bd8cdfb5e32
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-19 18:23:13 -0500

    nueva version

--- w3-school/tutorial/data-type.py
+++
@@ -1,8 +0,0 @@
-# Tipo de texto:	str
-# Tipos numéricos:	int, float, complex
-# Tipos de secuencia:	list, tuple, range
-# Tipo de mapeo:	dict
-# Tipos de conjuntos:	set,frozenset
-# Tipo booleano:	bool
-# Tipos binarios:	bytes, bytearray, memoryview
-# Ninguno Tipo:	NoneType
--- w3-school/tutorial/syntax.py
+++
@@ -1,9 +0,0 @@
-# Sangría en Python
-if 5 > 2:
-  print("Five is greater than two!")
-  print("Five is greater than two!")
-
-# Variables de Python
-x = 5
-y = "Hello, World"
-
