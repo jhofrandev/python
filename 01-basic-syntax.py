b3225e7f754a5fc074e18e39010364eec1282587
Jhofran Briceño <jhofran.dev@outlook.com>
2025-02-25 22:36:00 -0500

    basic syntax v1

---
+++ 01-basic-syntax.py
@@ -0,0 +1,29 @@
+# 1. Identación y bloques de código
+## Python utiliza espacios o tabulaciones para definir bloques (como funcionesm buncles o condiciones)
+## Error común: No mantener la identación consistente (generalmente 4 espacios por nivel)
+## Ejemplo:
+if 5 > 2:
+  print("5 es mayor que 2")
+
+
+# 2. Variables y Tipos de Datos
+## Variables: No se declaran con un tipo especidico, Python es de tipo dinámico.
+nombre = "Ana" # String (str)
+edad = 25 # Entero (int)
+altura = 1.75 # Flotante (float)
+es_estudiante = True # Booleano (bool)
+## Tipos básicos:
+### int, float, str, bool, None
+### Colecciones: list, tuple, dict, set
+## Mutabilidad:
+### Inmutables: int, str, tuble, bool
+### Mutables: list, dict, set
+
+
+# 3. Operadores
+## Aritméticos: +, -, *. /. // (división entera), % (módulo), ** (potencia)
+## Comparación: ==, !=, <, >, <=, >=
+## Lógicos: and, or, not
+## Identidad: is, is not (comprueban si dos variables son el mismo objeto)
+## Pertenencia: in, not in (para verificar elementos en colecciones)
+
