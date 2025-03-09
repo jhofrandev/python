fdfe71605616a89a7cac8e7c9e27899c26d48e39
Jhofran Briceño <jhofran.dev@outlook.com>
2025-03-09 13:05:09 -0500

    Update 02-variables-and-dataTypes.py

--- 02-variables-and-dataTypes.py
+++ 02-variables-and-dataTypes.py
@@ -43,3 +43,60 @@ __nombre__ = "Variable especial"
 #    - Ysa snake_case para nombres compuestos: mi_variable
 #    - Para constantes, usa MAYÚSCULAS: PI = 3.1416
 
+
+################################################################################
+## Tipos de datos Fundamentales
+################################################################################
+
+## Inmutables:
+# int: Números enteros (1, 100, -10)
+# float: Números decimales (3.14, -0.001)
+# str: Cadenas de texto ('Hola', 'Python')
+# bool: Va,ores booleanos (True, False)
+# tupla: Colecciones ordenadas inmutables((1, 2, 3))
+
+## MUtables:
+# list: Colecciones ordenadas mutables ([1, 2, 3])
+# dict: Diccionarios (pares clave-valor) ({'nombre': 'Juan'})
+# set: Conjuntos de elementos únicos ({1, 2, 3})
+
+################################################################################
+## Asignación Múltiple
+################################################################################
+
+# Python permite asignar valores a múltiples variables en una sola línea:
+x, y, z = 1, 2, 3
+
+# También puedes intercambiar valores fácilmente:
+a, b, = 10, 20
+a, b = b, a
+
+################################################################################
+## Ámbito de Variables (Scope)
+################################################################################
+
+## Variables Locales
+# Definidad dentro de una función, solo accesibles dentro de ella:
+def mi_funcion():
+  x = 10    # Variable local
+  print(x)
+
+## Variables Globales
+# Definidas fuera de  cualquier función, accesibles en todo el módulo:
+y = 20
+
+def otra_funcion():
+  print(y) # Accede a la variable global
+  # Para modificarla dentro de la función:
+  global y
+  y = 30
+
+## Variable No locales
+# Para acceder a variables de un ámbito externo (pero no global):
+def funcion_externa():
+  z = 10
+  def funcion_interna():
+    nonlocal z
+    z = 20
+  funcion_interna()
+  print(z)  # Imprime 20
